1b4dcb7998828da0f257559d79b01e1f50478497
MeryKitty <anhmdq99@gmail.com>
2021-05-07 18:20:20 +0700

    add javadoc, fix range test

--- src/main/java/io/github/merykitty/graalpng/Image.java
+++ src/main/java/io/github/merykitty/graalpng/Image.java
@@ -5,6 +5,7 @@ import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Objects;
 
 import org.graalvm.polyglot.*;
 
@@ -35,32 +36,85 @@ public class Image {
         this.data = data;
     }
 
+    /**
+     * Read the pixel of an image, the return value is an int which is represent the colour value of the
+     * pixel in ARGB order
+     *
+     * @param row The row of the pixel
+     * @param col The column of the pixel
+     * @return The colour value of the pixel, in ARGB order
+     */
     public int getPixel(int row, int col) {
-        return data[col * width + col];
+        Objects.checkIndex(row, height);
+        Objects.checkIndex(col, width);
+        return data[row * width + col];
     }
 
+    /**
+     * Put a colour value to a pixel at the specified position
+     *
+     * @param row The row of the pixel
+     * @param col The column of the pixel
+     * @param value The colour value of the pixel, in ARGB order
+     */
     public void putPixel(int row, int col, int value) {
+        Objects.checkIndex(row, height);
+        Objects.checkIndex(col, width);
         data[row * width + col] = value;
     }
 
+    /**
+     * Get the width of the image
+     *
+     * @return The width of the image
+     */
     public int width() {
         return width;
     }
 
+    /**
+     * Get the height of the image
+     *
+     * @return The height of the image
+     */
     public int height() {
         return height;
     }
 
+    /**
+     * Create an blank image with all pixel set to completely transparent
+     *
+     * @param width The width of the created image
+     * @param height The height of the create image
+     * @return The created image
+     */
     public static Image createImage(int width, int height) {
         int[] data = new int[width * height];
         return new Image(width, height, data);
     }
 
-    public void writeImage(Path path) {
-        var pathBuffer = Arrays.copyOf(path.toString().getBytes(StandardCharsets.US_ASCII), path.toString().length());
-        PNG_WRITE.executeVoid(pathBuffer, pathBuffer.length, this.width, this.height, this.data, ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN);
+    /**
+     * Serialise an image to a file specified by path
+     *
+     * @param path The path to the output file
+     * @throws IOException
+     */
+    public void writeImage(Path path) throws IOException {
+        try {
+            var pathBuffer = Arrays.copyOf(path.toString().getBytes(StandardCharsets.US_ASCII), path.toString().length());
+            PNG_WRITE.executeVoid(pathBuffer, pathBuffer.length, this.width, this.height, this.data, ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN);
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
     }
 
+    /**
+     * Read an image from disk
+     *
+     * @param path The path to the input file
+     * @return The image
+     * @throws IOException
+     */
     public static Image readImage(Path path) throws IOException {
         try (var inputStream = new DataInputStream(new FileInputStream(new File(path.toUri())))) {
             inputStream.readLong();
@@ -71,6 +125,8 @@ public class Image {
             var pathBuffer = Arrays.copyOf(path.toString().getBytes(StandardCharsets.US_ASCII), path.toString().length());
             PNG_READ.executeVoid(pathBuffer, pathBuffer.length, width, height, data, ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN);
             return new Image(width, height, data);
+        } catch (Exception e) {
+            throw new IOException(e);
         }
     }
 }
--- src/main/java/io/github/merykitty/graalpng/Test.java
+++ src/main/java/io/github/merykitty/graalpng/Test.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public class Test {
-    public static void main(String[] args, int l) {
-        var image = Image.createImage(1000, 1000);
+    public static void main(String[] args, long l) throws IOException {
+        var image = Image.createImage(2000, 1000);
         int red = 0xffff0000;
         for (int i = 0; i < 1000; i++) {
-            for (int j = 0; j < 1000; j++) {
-                if ((i + j) % 2 == 0) {
+            for (int j = 0; j < 2000; j++) {
+                if (i % 100 <= 50) {
                     image.putPixel(i, j, red);
                 }
             }
@@ -19,6 +19,20 @@ public class Test {
 
     public static void main(String[] args) throws IOException {
         var image = Image.readImage(Path.of("./data/test.png"));
+        for (int row = 0; row < image.height(); row++) {
+            for (int col = 0; col < image.width(); col++) {
+                int pixel = image.getPixel(row, col);
+                int alpha = pixel >>> 24;
+                int red = (pixel >>> 16) & 0xff;
+                int green = (pixel >>> 8) & 0xff;
+                int blue = pixel & 0xff;
+                red /= 2;
+                green /= 2;
+                blue /= 2;
+                pixel = (alpha << 24) | (red << 16) | (green << 8) | blue;
+                image.putPixel(row, col, pixel);
+            }
+        }
         image.writeImage(Path.of("./data/test1.png"));
     }
 }
